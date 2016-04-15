8c86722091372fffca6054043d969cf925586b3b
Gabe Frangakis <gabe@mira.co>
2016-04-14 20:17:11 -0400

    up to prob 30

--- src/fourclojure/core.clj
+++ src/fourclojure/core.clj
@@ -5,3 +5,39 @@
   "I don't do a whole lot ... yet."
   [& args]
   (println "Hello, World!"))
+
+(defn seqreverse
+  "Reverse a sequence without using reverse"
+  [seq]
+  (reduce conj () seq))
+
+(defn sumitallup
+  "Sum a sequence of numbers"
+  [seq]
+  (partial apply +))
+
+(defn fibonacci
+  "Generate the first n fibonacci numbers"
+  [n]
+  (loop [accum '(1 1) i 3]
+    (if (> i n)
+      accum
+      (recur (concat accum (list (apply + (take-last 2 accum)))) (inc i)))
+  )
+)
+
+(defn palindrome
+  "detect palindromes"
+  [seq]
+  (= (reverse (reverse seq)) (reverse seq))
+)
+
+(defn flatSeq
+  "flatten without flatten"
+  [n]
+  (remove sequential? (tree-seq sequential? seq n)))
+
+(defn getcaps
+  "filter only the capital letters"
+  [s]
+  (clojure.string/replace s #"(?![A-Z])." ""))
