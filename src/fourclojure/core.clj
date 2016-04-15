24cc709eec7182bb2dfc50c7bbc3bb579137f7f5
Gabe Frangakis <gabe@mira.co>
2016-04-15 19:05:18 -0400

    problem 40

--- src/fourclojure/core.clj
+++ src/fourclojure/core.clj
@@ -41,3 +41,44 @@
   "filter only the capital letters"
   [s]
   (clojure.string/replace s #"(?![A-Z])." ""))
+
+(defn compress-seq
+  "Remove consecutive duplicates"
+  [s]
+  (reverse (reduce (fn [accum x]
+    (if (not= (first accum) x)
+      (cons x accum)
+      accum))
+    '()
+    s
+  ))
+)
+
+(defn pack-seq
+  "Pack consecutive duplicates into sublists"
+  [s]
+  (reverse (reduce (fn [accum x]
+    (if (= (ffirst accum) x)
+      (cons (cons x (first accum)) (rest accum))
+      (cons (list x) accum)
+    )) '() s))
+)
+
+(comment "Duplicate sequence")
+#(mapcat (fn [x] (list x x)) %)
+
+(comment "Replicate a sequence")
+(fn [x y] (mapcat (fn [e] (repeat y e)) x))
+
+(comment "Implement range")
+(fn [x y] (take (- y x) (iterate inc x)))
+
+(comment "Max Value")
+(fn [& body] (reduce (fn [x y] (if (> x y) x y)) body))
+
+(comment "Interleave")
+(fn [a b] (flatten
+  (for [i (range (min (count a) (count b)))]
+    (list (a i) (b i))
+  ))
+)
